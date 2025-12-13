ed34829cceb0149826fe8de804c71e73440ce212
darrylemiles <dmilesbacay@gmail.com>
2025-12-14 07:03:00 +0800

    feat: restructure Home component layout with Grid for improved responsiveness

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,16 @@
-import { Container } from '@mui/material'
+import { Container, Grid } from '@mui/material'
+
 import React from 'react'
+import AboutMe from '../sections/home/AboutMe'
 
 const Home: React.FC = () => {
   return (
-    <Container>
-      aa
+    <Container sx={{ py: 2 }}>
+      <Grid container spacing={2} columns={12}>
+        <Grid size={{ xs: 12, md: 4 }}>
+          <AboutMe />
+        </Grid>
+      </Grid>
     </Container>
   )
 }
