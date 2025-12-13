4be101aedc4846dfe84b988b6dd268075dbe2016
darrylemiles <dmilesbacay@gmail.com>
2025-12-14 07:03:06 +0800

    feat: add MainCard component for consistent card layout

---
+++ client/src/components/MainCard.tsx
@@ -0,0 +1,24 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+type MainCardProps = {
+  children: React.ReactNode;
+};
+
+const MainCard: React.FC<MainCardProps> = ({ children }) => {
+  return (
+    <Box
+      sx={{
+        background: theme => theme.palette.background.paper,
+        border: '1px solid rgba(255, 255, 255, .08)',
+        borderRadius: 2.5,
+        p: 3,
+        boxShadow: '0 4px 16px rgba(0, 0, 0, 0.4)',
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default MainCard
