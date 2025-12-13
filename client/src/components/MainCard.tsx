e40df038fbaef36e8a6620418f71f6a7301395b9
darrylemiles <dmilesbacay@gmail.com>
2025-12-14 07:18:48 +0800

    refactor: change type alias to interface for MainCardProps and WrapperProps

--- client/src/components/MainCard.tsx
+++ client/src/components/MainCard.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material'
 import React from 'react'
 
-type MainCardProps = {
+interface MainCardProps {
   children: React.ReactNode;
 };
 
--- client/src/layout/Wrapper.tsx
+++ client/src/layout/Wrapper.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import React from 'react'
 
-type WrapperProps = {
+interface WrapperProps {
   children: React.ReactNode;
 }
 
