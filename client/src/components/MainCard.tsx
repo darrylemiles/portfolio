cdaae1adc447a8a42bce5858330e75dc213e91b4
darrylemiles <dmilesbacay@gmail.com>
2025-12-14 08:25:52 +0800

    feat: add title prop to MainCard component for enhanced display

--- client/src/components/MainCard.tsx
+++ client/src/components/MainCard.tsx
@@ -1,11 +1,15 @@
-import { Box } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 interface MainCardProps {
-  children: React.ReactNode;
+  children: React.ReactNode,
+  title?: {
+    text: string,
+    icon?: React.ReactNode,
+  }
 };
 
-const MainCard: React.FC<MainCardProps> = ({ children }) => {
+const MainCard: React.FC<MainCardProps> = ({ children, title }) => {
   return (
     <Box
       sx={{
@@ -16,6 +20,18 @@ const MainCard: React.FC<MainCardProps> = ({ children }) => {
         boxShadow: '0 4px 16px rgba(0, 0, 0, 0.4)',
       }}
     >
+      {title && (
+        <Stack direction="row" alignItems="center" spacing={2} mb={2}>
+          <Box>
+            {title.icon}
+          </Box>
+
+          <Typography variant='h5'>
+            {title.text}
+          </Typography>
+        </Stack>
+      )}
+
       {children}
     </Box>
   );
