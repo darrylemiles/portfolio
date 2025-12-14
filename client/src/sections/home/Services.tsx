a1595188e40b0a9ab83139653a81b60ab96c8d60
darrylemiles <dmilesbacay@gmail.com>
2025-12-14 08:43:56 +0800

    wip: implement Services component with dynamic service listing and layout

--- client/src/sections/home/Services.tsx
+++ client/src/sections/home/Services.tsx
@@ -2,11 +2,24 @@ import React from 'react'
 import MainCard from '../../components/MainCard'
 import { SettingOutlined } from '@ant-design/icons'
 import AnimateButton from '../../components/extended/AnimateButton'
+import Chip from '../../components/Chip'
+import { Box } from '@mui/material'
 
 const Services: React.FC = () => {
+  const services = [
+    'Full Stack Web Development',
+    'Mobile Apps',
+    'UI/UX Design',
+    'SEO Optimization'
+  ]
+
   return (
     <MainCard title={{ text: 'Services', icon: <AnimateButton type='rotate'> <SettingOutlined style={{ fontSize: 23 }} /> </AnimateButton> }}>
-      Services
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1 }}>
+        {services.map((service, index) => (
+          <Chip text={service} key={index} />
+        ))}
+      </Box>
     </MainCard >
   )
 }
