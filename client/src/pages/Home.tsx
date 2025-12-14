7ba2455722f64cb3fa2bef2a9934385848443ec7
darrylemiles <dmilesbacay@gmail.com>
2025-12-14 08:26:26 +0800

    refactor: reorganize imports and enhance layout structure in Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,14 @@
 import { Grid } from '@mui/material'
 
 import React from 'react'
-import AboutMe from '../sections/home/AboutMe'
+
+// components
 import Container from '../components/Container'
+import Divider from '../components/Divider'
+
+// sections
+import AboutMe from '../sections/home/AboutMe'
+import Services from '../sections/home/Services'
 
 const Home: React.FC = () => {
   return (
@@ -10,6 +16,32 @@ const Home: React.FC = () => {
       <Grid container spacing={2} columns={12}>
         <Grid size={{ xs: 12, md: 4 }}>
           <AboutMe />
+          <Divider />
+
+          <Services />
+          <Divider />
+
+          <Services />
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 4 }}>
+          <AboutMe />
+          <Divider />
+
+          <Services />
+          <Divider />
+
+          <Services />
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 4 }}>
+          <AboutMe />
+          <Divider />
+
+          <Services />
+          <Divider />
+
+          <Services />
         </Grid>
       </Grid>
     </Container>
