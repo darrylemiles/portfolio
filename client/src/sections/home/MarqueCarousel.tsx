b7153d26d4f97aeddb2f1758bee3c7d0772747b9
darrylemiles <dmilesbacay@gmail.com>
2026-01-07 05:02:16 +0800

    refactor: extract carousel rendering logic into a separate function for clarity

--- client/src/sections/home/MarqueCarousel.tsx
+++ client/src/sections/home/MarqueCarousel.tsx
@@ -2,7 +2,30 @@ import { Box } from '@mui/material'
 import React from 'react'
 import Marquee from 'react-fast-marquee'
 
+const items = [
+  'Information Technology',
+  'Full Stack Web Developer',
+  'STI College Dasmarinas',
+];
+
+const classes = [
+  'marquee-text marquee-text--blue',
+  'marquee-text marquee-text--green',
+  'marquee-text marquee-text--purple',
+  'marquee-text marquee-text--yellow',
+  'marquee-text marquee-text--red',
+];
+
+const renderCarousel = () => {
+  return items.map((text, idx) => (
+    <Box component='span' key={idx} className={classes[idx % classes.length]} style={{ marginInline: 24 }}>
+      {text}
+    </Box>
+  ));
+}
+
 const MarqueCarousel: React.FC = () => {
+
   return (
     <Box
       sx={{
@@ -10,25 +33,7 @@ const MarqueCarousel: React.FC = () => {
         py: 5,
       }}>
       <Marquee pauseOnHover gradient gradientWidth={60} speed={70} autoFill>
-        {(() => {
-          const items = [
-            'Information Technology',
-            'Certified Full Stack Web Developer',
-            'STI College Dasmarinas',
-          ];
-          const classes = [
-            'marquee-text marquee-text--blue',
-            'marquee-text marquee-text--green',
-            'marquee-text marquee-text--purple',
-            'marquee-text marquee-text--yellow',
-            'marquee-text marquee-text--red',
-          ];
-          return items.map((text, idx) => (
-            <Box component='span' key={idx} className={classes[idx % classes.length]} style={{ marginInline: 24 }}>
-              {text}
-            </Box>
-          ));
-        })()}
+        {renderCarousel()}
       </Marquee>
     </Box>
   )
