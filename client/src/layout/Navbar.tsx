1b0966a84ee603d42a4fa184c91ad09dcbd37282
darrylemiles <dmilesbacay@gmail.com>
2026-01-11 12:11:33 +0800

    refactor: remove CV download and open buttons from Navbar component

--- client/src/layout/Navbar.tsx
+++ client/src/layout/Navbar.tsx
@@ -1,22 +1,11 @@
 import { Button, Stack, Tooltip } from '@mui/material'
-import { FaDownload, FaExternalLinkAlt, FaHome } from 'react-icons/fa'
+import { FaHome } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom';
 
 import React from 'react'
-import cvPdf from '../assets/Darryle Miles CV.pdf';
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
-  const downloadCv = () => {
-    const a = document.createElement('a');
-    a.href = cvPdf;
-    a.download = 'Darryle Miles CV.pdf';
-    a.click();
-  };
-
-  const openInNewTab = () => {
-    window.open(cvPdf, '_blank', 'noopener,noreferrer');
-  };
 
   return (
     <Stack
@@ -41,34 +30,6 @@ const Navbar: React.FC = () => {
         >
           Home
         </Button>
-      </Stack>
-      <Stack
-        direction={{ xs: 'column', sm: 'row' }}
-        spacing={1}
-        sx={{ width: { xs: '100%', sm: 'auto' } }}
-      >
-        <Tooltip title="Download PDF">
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<FaDownload />}
-            onClick={downloadCv}
-            sx={{ width: { xs: '100%', sm: 'auto' } }}
-          >
-            Download
-          </Button>
-        </Tooltip>
-        <Tooltip title="Open in new tab">
-          <Button
-            variant="outlined"
-            color="inherit"
-            startIcon={<FaExternalLinkAlt />}
-            onClick={openInNewTab}
-            sx={{ width: { xs: '100%', sm: 'auto' } }}
-          >
-            Open
-          </Button>
-        </Tooltip>
       </Stack>
     </Stack>
   )
