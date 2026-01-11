24edea9f1bcff9a16251b937151a40be79e25b49
darrylemiles <dmilesbacay@gmail.com>
2026-01-11 13:21:19 +0800

    refactor: remove unused Tooltip import from Navbar component

--- client/src/layout/Navbar.tsx
+++ client/src/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack, Tooltip } from '@mui/material'
+import { Button, Stack } from '@mui/material'
 import { FaHome } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom';
 
